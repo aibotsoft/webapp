8e1c485a5b7b32cb4aa24b10c06e6c31cfbd22ce
aibotsoft <aibotsoft@gmail.com>
2020-09-21 23:49:09 +0300

    feat: first handler

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handlerFuck(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+
+}
 func main() {
-	fmt.Printf("hello world")
+	http.HandleFunc("/", handlerFuck)
+	fmt.Printf("http://localhost:3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
