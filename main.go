0f2b7097762fe7c93af56e03c917d668a752f680
aibotsoft <aibotsoft@gmail.com>
2020-09-22 21:31:03 +0300

    feat: not found page

--- main.go
+++ main.go
@@ -20,10 +20,16 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	_, _ = fmt.Fprint(w, "<h1>Frequently Asked Questions Page</h1>")
 }
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	_, _ = fmt.Fprint(w, "<h1>Not found page 404</h1>")
+}
 
 func main() {
 	//fmt.Printf("http://localhost:3000")
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
