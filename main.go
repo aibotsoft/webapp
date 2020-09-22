3c03c81f5c27a3ad30c30170b8fe60dab2467e5a
aibotsoft <aibotsoft@gmail.com>
2020-09-22 21:11:17 +0300

    feat: faq page

--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 	"net/http"
 )
 
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprint(w, "<h1>Fuck off</h1>")
-
-}
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/html")
-	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site change!</h1>")
-}
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	_, _ = fmt.Fprint(w, "<h1>Home page!</h1>")
@@ -24,11 +16,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	_, _ = fmt.Fprint(w, "<h1>Contact page</h1>")
 }
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	_, _ = fmt.Fprint(w, "<h1>Frequently Asked Questions Page</h1>")
+}
 
 func main() {
 	//fmt.Printf("http://localhost:3000")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
