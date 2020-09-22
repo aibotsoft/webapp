2683504cbb5909be44292ffce8918141a5b9041f
aibotsoft <aibotsoft@gmail.com>
2020-09-22 15:09:16 +0300

    feat: path play

--- main.go
+++ main.go
@@ -5,14 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFuck(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("someone visit our page")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
-	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site change!</h1>")
+	_, _ = fmt.Fprint(w, r.URL.Path)
+	fmt.Println(r.URL)
+	if r.URL.Path == "/" {
+		_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site change!</h1>")
+	} else if r.URL.Path == "/fuck" {
+		_, _ = fmt.Fprint(w, "<h1>Fuck off</h1>")
+	}
 
 }
 func main() {
-	http.HandleFunc("/", handlerFuck)
+	http.HandleFunc("/", handlerFunc)
 	fmt.Printf("http://localhost:3000")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
